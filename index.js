54284466456eb8ea5cd1993a7d8cf1bda8e099f2
sajib689 <jibonraja11@gmail.com>
2024-02-11 11:05:13 +0600

    fff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 btn.addEventListener('click', function (e) {
-    alert('tor bap rea bollbo na ki')
+    document.body.style.backgroundColor = 'red';
 });

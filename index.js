0864408e952304e964bc3b34405d9f4402a36cf8
sajib689 <jibonraja11@gmail.com>
2024-02-11 11:55:03 +0600

    done

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const text = document.getElementById('text');
 btn.addEventListener('click', function (e) {
     document.body.style.backgroundColor = 'red';
 });
+// 04
+
+const makePink = document.getElementById('make-pink')
+
+makePink.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'pink'
+})
+// 04 another option
+
+const makeGreen = document.getElementById('make-green')
+
+makeGreen.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green'
+});
+// option final
+
+document.getElementById('make-golden').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'golden'
+})
